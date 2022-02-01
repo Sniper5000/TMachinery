71ce11d7c3c605fe006698437d099da13053ced4
Sniper5000 <bryan_davadi6@hotmail.com>
2022-01-31 20:43:49 -0600

    Removed IE section for build

--- src/main/java/com/blusunrize/immersiveengineering/api/wires/utils/IElectricDamageSource.java
+++
@@ -1,9 +0,0 @@
-package blusunrize.immersiveengineering.api.wires.utils;
-
-import net.minecraft.world.entity.Entity;
-
-public interface IElectricDamageSource
-{
-	boolean apply(Entity e);
-	float getDamage();
-}
